bb41a97d386cb41c9810363b8823cbfc613a5d92
Vidhyathegreat <95758776+Vidhyathegreat@users.noreply.github.com>
2022-04-01 19:13:55 +0530

    Add files via upload

---
+++ ConditionalStatements!.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int marks;
+	printf("Enter marks: ");
+	scanf("%d",&marks);
+	if (marks>=85 && marks<=100){
+		printf("GRADE A");
+	}
+	else if(marks>=70 && marks<=84){
+		printf("GRADE B");
+	}
+	else if(marks>=55 && marks<=69){
+		printf("GRADE C");
+	}
+	else if(marks>=40 && marks<=54){
+		printf("GRADE D");
+	}
+	else if (marks<40){
+		printf("GRADE F");
+	} else {
+		printf("GRADE Undertermined");
+	}
+return 0;
+}
+		
+	
+
+
