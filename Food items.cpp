aacf47a7eb1d402578afa72ef21b0d1af2202da0
Vidhyathegreat <95758776+Vidhyathegreat@users.noreply.github.com>
2022-04-11 11:07:43 +0530

    Add files via upload

---
+++ Food items.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int No;
+	printf("Enter a No: ");
+	scanf("%d",&No);
+	if (No==1){
+		printf("Food item: Pizza\n");
+		printf("Price:239");
+	}
+	else if(No==2){
+		printf("Food item: Burger\n");
+		printf("Price:129");
+	}
+	else if(No==3){
+		printf("Food item: Pasta\n");
+		printf("Price:179");
+	}
+	else if(No==4){
+		printf("Food item: French Fries\n");
+		printf("Price:99");
+	}
+	else if(No==5){
+		printf("Food item: Sandwich\n");
+		printf("Price:149");
+	}
+	else{
+		printf("Use the default case");
+	}
+	return 0;	
+}
+	
+		
+	
+		
+	
+
+	
+	
+
